31cccb4dd9d2a2830e08cfaf98f8554589a6a9c4
A Behesti <albehestione@gmail.com>
2025-10-15 23:54:06 +0600

    added private route

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import Loader from "../pages/Shared/Loader";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const lacation = useLocation();
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
