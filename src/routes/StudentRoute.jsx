92e8475de124ee41a7931ebf1543e186f78d270d
A Behesti <albehestione@gmail.com>
2025-10-20 23:56:04 +0600

    updated the code

--- src/routes/StudentRoute.jsx
+++ src/routes/StudentRoute.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import useStudent from "../hooks/useStudent";
